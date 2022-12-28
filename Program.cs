2dc0294704b98ffbba4672ac71472f8eaec5ac94
mohamed36363 <112127545+mohamed36363@users.noreply.github.com>
2022-12-28 21:43:45 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿namespace discreteprefect
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter Starting Number: ");
+            int startNumber = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter Ending Number: ");
+            int endNumber = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Below are the perfect number between " + startNumber + " and " + endNumber);
+
+            for (int i = startNumber; i <= endNumber; i++)
+            {
+                int sum = 0;
+                for (int j = 1; j < i; j++)
+                {
+                    if (i % j == 0)
+                        sum = sum + j;
+                }
+                if (sum == i)
+                    Console.WriteLine(i);
+            }
+        }
+    }
+}
